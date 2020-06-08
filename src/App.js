663613cb0a0882538d0d3691a530c12dd63cad58
eshoiza <engrjayt200@gmail.com>
2020-06-08 21:44:28 +0100

    updated login component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import {BrowserRouter as Router, Switch, Link} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
  import './App.css';
-export default function App() {
-  return (
+
+ import Login from "./components/Login.js";
+ import SignUp from "./components/Signup.js";
+
+ function App() {
+  return ( 
+    <Router>
     <div className="App">
-<h3>Build sign Up & Login UI Template</h3>   
- </div>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>jamesoyanna.ga</Link>
+        <div className="collapse navbar-collapse" id="navbarToggle">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to={"/sign-in"}>Login</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"sign-up"}>Sign up</Link>
+            </li>
+          </ul>
+        </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div>
+    </Router>
   );
 }
+export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
-export default function Login() {
+ function Login() {
     return (
-        <div>
-          <h2>Hello from Login</h2>  
+        <form>
+          <h3>Sign In</h3>
+
+      <div className="form-group">
+        <label>Email address</label>
+        <input type="email" className="form-control" placeholder="Enter email" />
+        </div>    
+
+        <div className="form-group">
+          <label>Password</label>
+          <input type="password" className="form-control" placeholder="Enter password" />
+
+          <div className="form-group"></div>
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="customCheck1" />
+            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+          </div>
         </div>
+
+        <button type="submit" className="btn btn-primary btn-block">Submit</button>
+        <p className="forgot-password text-right">forgot
+        <a href="#">password?</a>
+        </p>
+        </form>
     )
 }
+export default Login;
