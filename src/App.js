156888006b16d670cbf195b0a0c4749e736609e9
eshoiza <engrjayt200@gmail.com>
2020-06-08 12:18:26 +0100

    project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Switch, Link} from "react-router-dom";
+ import './App.css';
+export default function App() {
+  return (
+    <div className="App">
+<h3>Build sign Up & Login UI Template</h3>   
+ </div>
+  );
+}
---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div>
+          <h2>Hello from Login</h2>  
+        </div>
+    )
+}
